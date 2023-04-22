583c3ee9f2022090f6fdcfd6293c567f7eeb44ee
LATerada <larissaakiterada@gmail.com>
2023-04-22 12:07:25 -0300

    :card_file_box: Add Tables users, posts, likes_dislikes

--- src/database/labook.sql
+++ src/database/labook.sql
@@ -5,66 +5,28 @@ CREATE TABLE
         id TEXT PRIMARY KEY UNIQUE NOT NULL,
         name TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        created_at TEXT DEFAULT (DATETIME()) NOT NULL
     );
 
 CREATE TABLE
-    tasks (
+    posts (
         id TEXT PRIMARY KEY UNIQUE NOT NULL,
-        title TEXT NOT NULL,
-        description TEXT NOT NULL,
+        creator_id TEXT NOT NULL,
+        content TEXT NOT NULL,
+        likes INTEGER DEFAULT(0) NOT NULL,
+        dislikes INTEGER DEFAULT(0) NOT NULL,
         created_at TEXT DEFAULT (DATETIME()) NOT NULL,
-        status INTEGER DEFAULT (0) NOT NULL
+        updated_at TEXT DEFAULT (0) NOT NULL,
+        FOREIGN KEY (creator_id) REFERENCES users(id)
     );
 
 CREATE TABLE
-    users_tasks (
+    likes_dislikes (
         user_id TEXT NOT NULL,
-        task_id TEXT NOT NULL,
+        post_id TEXT NOT NULL,
+        like INTEGER DEFAULT (0) NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users (id),
-        FOREIGN KEY (task_id) REFERENCES tasks (id)
+        FOREIGN KEY (post_id) REFERENCES posts (id)
     );
-
-INSERT INTO
-    users (id, name, email, password)
-VALUES (
-        "f001",
-        "Fulano",
-        "fulano@email.com",
-        "fulano123"
-    ), (
-        "f002",
-        "Beltrana",
-        "beltrana@email.com",
-        "beltrana00"
-    );
-
-INSERT INTO
-    tasks (id, title, description)
-VALUES (
-        "t001",
-        "Implementar o header",
-        "Criar o componente Header do site"
-    ), (
-        "t002",
-        "Implementar o footer",
-        "Criar o componente Footer do site"
-    ), (
-        "t003",
-        "Testar site",
-        "Teste de usabilidade de todo o site"
-    ), (
-        "t004",
-        "Deploy do site",
-        "Subir o site no surge"
-    );
-
-INSERT INTO
-    users_tasks (user_id, task_id)
-VALUES ("f001", "t001"), ("f002", "t002"), ("f001", "t003"), ("f002", "t003");
-
-SELECT * FROM users;
-
-SELECT * FROM tasks;
-
-SELECT * FROM users_tasks;
