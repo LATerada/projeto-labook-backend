6d1344d08f760372707ac6d575e0144f963c16ec
LATerada <larissaakiterada@gmail.com>
2023-04-22 12:45:14 -0300

    :truck: Mode types.ts to src

--- src/database/PostDatabase.ts
+++ src/database/PostDatabase.ts
@@ -1,5 +1,5 @@
 import { BaseDatabase } from "./BaseDatabase";
-import { PostDB } from "./types";
+import { PostDB } from "../types";
 
 export class PostDatabase extends BaseDatabase {
   public static TABLE_POSTS = "posts";
